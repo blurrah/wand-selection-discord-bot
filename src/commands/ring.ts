f475774d52efe2295a0c06a786052636b9356d4f
Boris Besemer <borisbesemer@gmail.com>
2023-06-18 22:04:57 +0200

    feat(ring): add better logic for handling wand selection, getting there

--- src/commands/ring.ts
+++ src/commands/ring.ts
@@ -1,23 +1,42 @@
 import {
   ActionRowBuilder,
+  BaseInteraction,
   ButtonBuilder,
   ButtonInteraction,
   ButtonStyle,
   CacheType,
   ChatInputCommandInteraction,
+  Collection,
+  CollectorFilter,
   ComponentType,
   SlashCommandBuilder,
 } from "discord.js";
 import { wandSelector } from "lib/wand-selector";
 import messages from "../../messages.json";
 
-const handleStartButtonInteraction = async (
+const createFilter =
+  (
+    interaction: BaseInteraction
+  ): CollectorFilter<
+    [
+      ButtonInteraction<CacheType>,
+      Collection<string, ButtonInteraction<CacheType>>
+    ]
+  > =>
+  async (collectInteraction) => {
+    await collectInteraction.deferUpdate();
+    return collectInteraction.user.id === interaction.user.id;
+  };
+
+/**
+ * Handles the wand selection interaction. Can be recursively used until a wand has been chosen.
+ */
+const handleWandSelectionInteraction = async (
   baseInteraction: ChatInputCommandInteraction,
-  collectInteraction: ButtonInteraction<CacheType>
+  interaction: ButtonInteraction<CacheType>
 ) => {
-  console.log("hello");
-  if (collectInteraction.user.id !== baseInteraction.user.id) {
-    await collectInteraction.reply({
+  if (interaction.user.id !== baseInteraction.user.id) {
+    await interaction.reply({
       content: messages.wrongUserInteraction,
       ephemeral: true,
     });
@@ -25,15 +44,45 @@ const handleStartButtonInteraction = async (
     return;
   }
 
+  const filter = createFilter(baseInteraction);
+
   const { wood, core, length } = await wandSelector();
 
-  await collectInteraction.reply({
-    content: messages.getsAWand
+  const swing = new ButtonBuilder()
+    .setCustomId("swing-wand")
+    .setLabel("Swing the wand")
+    .setStyle(ButtonStyle.Primary);
+
+  const row = new ActionRowBuilder<ButtonBuilder>().addComponents(swing);
+
+  const message = await interaction.editReply({
+    content: messages.testTheWand
       .replace("{user}", baseInteraction.user.toString())
       .replace("{wood}", wood)
       .replace("{core}", core)
       .replace("{length}", (length / 10).toString()),
+    components: [row],
   });
+
+  try {
+    const collectInteraction = await message.awaitMessageComponent({
+      filter,
+      componentType: ComponentType.Button,
+      time: 60000,
+    });
+
+    if (Math.random() < 0.5) {
+      await collectInteraction.editReply({
+        content: "You got a wand!!!!!!!!!!!",
+        components: [],
+      });
+
+      return;
+    }
+    await handleWandSelectionInteraction(baseInteraction, collectInteraction);
+  } catch (error) {
+    console.warn(`Something went wrong: ${error}`);
+  }
 };
 
 export default {
@@ -56,18 +105,22 @@ export default {
       deny
     );
 
-    const collector = interaction.channel?.createMessageComponentCollector({
-      componentType: ComponentType.Button,
-      time: 15000,
-    });
+    const filter = createFilter(interaction);
 
-    collector?.on("collect", (collectInteraction) =>
-      handleStartButtonInteraction(interaction, collectInteraction)
-    );
-
-    await interaction.reply({
+    const message = await interaction.reply({
       content: messages.startSelection,
       components: [row],
     });
+
+    try {
+      const collectInteraction = await message.awaitMessageComponent({
+        filter,
+        componentType: ComponentType.Button,
+        time: 60000,
+      });
+      await handleWandSelectionInteraction(interaction, collectInteraction);
+    } catch (error) {
+      console.warn(`Something went wrong: ${error}`);
+    }
   },
 };
